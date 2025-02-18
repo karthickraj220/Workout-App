aff75f50e3d7dee4ba998b4afb0e437c431908f5
karthick <karthickraja220@gmail.com>
2025-02-18 18:42:09 +0530

    Initial commit

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,30 @@
+import { FaTrash } from 'react-icons/fa';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Number of reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+      <FaTrash onClick={handleClick} style={{ cursor: 'pointer', color: 'red' }} />
+    </div>
+  )
+}
+
+export default WorkoutDetails
